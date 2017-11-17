a5f9295628c15d8e0e9fcbe130233022c0ce9ac6
ittipong2535 <33741925+ittipong2535@users.noreply.github.com>
2017-11-17 20:27:19 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -44,4 +44,30 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 $result = curl_exec($ch);
 curl_close ($ch);
+
+$strAccessToken = "SHuI9jI94ZgsjRipHJB3oUkh3kbVBNbkQtEKXBPtxEkY7eY9hzU+TOIWHnW7rwHzWho/2mjToC0dnn1Mx3irTc4SFUSlnAC4WAitVPfkzfszr00vhkvQUMUI97dOVa7PmpXSPaAspkOBErvXDnLEIAdB04t89/1O/w1cDnyilFU=";
+ 
+$strUrl = "https://api.line.me/v2/bot/message/push";
+ 
+$arrHeader = array();
+$arrHeader[] = "Content-Type: application/json";
+$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+ 
+$arrPostData = array();
+$arrPostData['to'] = "Ua0ed93023aec653d3d200a441e07f6d6";
+$arrPostData['messages'][0]['type'] = "text";
+$arrPostData['messages'][0]['text'] = "นี้คือการทดสอบ Push Message";
+ 
+ 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$strUrl);
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$result = curl_exec($ch);
+curl_close ($ch);
+
 ?>
