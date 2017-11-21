ce49619639adb9784873b9dab28ad9fac8765101
ittipong2535 <33741925+ittipong2535@users.noreply.github.com>
2017-11-21 23:34:18 +0700

    Update api.php

--- api.php
+++ api.php
@@ -44,7 +44,7 @@ if (strpos($_msg, 'สอน47') !== false) {
     $arrPostData = array();
     $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
     $arrPostData['messages'][0]['type'] = "text";
-    $arrPostData['messages'][0]['text'] = 'ขอบคุณครับที่สอน';
+    $arrPostData['messages'][0]['text'] = 'บอท';
   }
 }else{
   if($isData >0){
